862d8deaf93a4b6a877982624c634c83b96e6233
MaAnka324 <matviychuk.anna324@gmail.com>
2023-02-18 19:26:36 +0200

    test22

--- src/s2-homeworks/hw03/HW3.tsx
+++ src/s2-homeworks/hw03/HW3.tsx
@@ -4,9 +4,9 @@ import s2 from '../../s1-main/App.module.css'
 import GreetingContainer from './GreetingContainer'
 
 /*
-* 1 - описать тип UserType
-* 2 - указать нужный тип в useState с users
-* 3 - дописать типы и логику функции pureAddUserCallback и проверить её тестами
+* 1 - описать тип UserType  +
+* 2 - указать нужный тип в useState с users +
+* 3 - дописать типы и логику функции pureAddUserCallback и проверить её тестами +
 * 4 - в файле GreetingContainer.tsx дописать типизацию пропсов
 * 5 - в файле GreetingContainer.tsx указать нужные типы в useState с name и error
 * 6 - в файле GreetingContainer.tsx дописать тип и логику функции setNameCallback
@@ -19,23 +19,27 @@ import GreetingContainer from './GreetingContainer'
 
 // types
 export type UserType = {
-    _id: any // need to fix any
-    name: any // need to fix any
+    _id: string // need to fix any +
+    name: string // need to fix any +
 }
 
-export const pureAddUserCallback = (name: any, setUsers: any, users: any) => { // need to fix any
-    const user = {
+export const pureAddUserCallback = (name: string, setUsers: any, users: UserType[]) => { // need to fix any
+    const user: UserType = {
+        _id: v1(),
+        name: name
         // need to fix
-        // нужно создать новый объект соответствующий типу UserType
-        // не забыть   сгенеририовать _id и вставить пришедший в функцию name
+        // нужно создать новый объект соответствующий типу UserType +
+        // не забыть   сгенеририовать _id и вставить пришедший в функцию name +
     }
-    //засетайЮзеров([...старые users, и наш новый user])
+    const updatedTasks: UserType[] = [...users, user]
+    setUsers(updatedTasks)
+    //засетайЮзеров([...старые users, и наш новый user]) +
 }
 
 const HW3 = () => {
-    const [users, setUsers] = useState<any>([]) // need to fix any
+    const [users, setUsers] = useState<UserType[]>([]) // need to fix any +
 
-    const addUserCallback = (name: any) => { // need to fix any
+    const addUserCallback = (name: string) => { // need to fix any +
         pureAddUserCallback(name, setUsers, users)
     }
 
