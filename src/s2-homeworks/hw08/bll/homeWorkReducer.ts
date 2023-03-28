7ea518b56312bb1e7b1c52e10ac100e8fae73dc3
MaAnka324 <matviychuk.anna324@gmail.com>
2023-03-28 14:45:24 +0300

    test22

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -5,14 +5,26 @@ type ActionType =
     | { type: 'check'; payload: number }
 
 export const homeWorkReducer = (state: UserType[], action: ActionType): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
+    switch (action.payload) {
+        case 'up': { // by name
             // sort() создаёт новый массив? или нужно в ручную?...
-            return state
-
-            // need to fix
+            let newState = [...state]
+            newState.sort((a, b) => a.name > b.name
+                ? 1
+                : -1
+            )
+            return newState
         }
-        case 'check': {
+        case 'down': { // by name
+            // sort() создаёт новый массив? или нужно в ручную?...
+            let newState = [...state]
+            newState.sort((a, b) => a.name > b.name
+                ? -1
+                : 1
+            )
+            return newState
+        }
+        case 18: {
             // filter() создаёт новый массив? или нужно в ручную?...
             return state.filter(a => a.age >= 18)
             // need to fix +
@@ -21,3 +33,20 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): any => {
             return state
     }
 }
+
+
+// export const homeWorkReducer = (state: UserType[], action: ActionType): any => { // need to fix any
+//     switch (action.type) {
+//         case 'sort': { // by name
+//             // sort() создаёт новый массив? или нужно в ручную?...
+//             return state // need to fix
+//         }
+//         case 'check': {
+//             // filter() создаёт новый массив? или нужно в ручную?...
+//             return state.filter(a => a.age >= 18)
+//             // need to fix +
+//         }
+//         default:
+//             return state
+//     }
+// }
