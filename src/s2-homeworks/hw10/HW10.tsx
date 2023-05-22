798253dc7888e2ba47b76ccfe6235cd607556b33
MaAnka324 <matviychuk.anna324@gmail.com>
2023-05-22 14:40:00 +0300

    test22

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -14,13 +14,22 @@ import {Loader} from './Loader'
 * */
 
 const HW10 = () => {
+    const dispatch = useDispatch()
+    const isLoading = useSelector<AppStoreType, boolean>(state => state.loading.isLoading)
     // useSelector, useDispatch // пишет студент
-    const isLoading = false
+    //const isLoading = false
 
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
         // dispatch
 
-        // setTimeout
+        const action = loadingAC(true)
+        dispatch(action)
+
+        //setTimeout
+        setTimeout(() => {
+            dispatch(loadingAC(false))
+        }, 1500);
+
     }
 
     return (
